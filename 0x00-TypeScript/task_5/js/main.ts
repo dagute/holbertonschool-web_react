116715bd8215a230185d818f8ebfad8c35fd8556
dagute <dagute28@gmail.com>
2021-02-10 17:31:15 +0000

    update task 5

--- 0x00-TypeScript/task_5/js/main.ts
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -1,12 +1,25 @@
-export interface MajorCredits {
+interface MajorCredits {
   credits: number;
+  _majorBrand: void;
 }
-export interface MinorCredits {
-credits: number;
+
+interface MinorCredits {
+  credits: number;
+  _minorBrand: void;
 }
-export const sumMajorCredits = (subject1: number, subject2: number): MajorCredits => {
-return {credits: subject1 + subject2};
-}; 
-export const sumMinorCredits = (subject1: number, subject2: number): MinorCredits => {
-return {credits: subject1 + subject2};
-}; 
+
+const course1: MajorCredits = { credits: 3 } as MajorCredits;
+const course2: MajorCredits = { credits: 3 } as MajorCredits;
+const course3: MinorCredits = { credits: 1 } as MinorCredits; 
+const course4: MinorCredits = { credits: 1 } as MinorCredits;
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
+  return { credits: subject1.credits + subject2.credits} as MajorCredits;
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
+  return { credits: subject1.credits + subject2.credits} as MinorCredits;
+}
+
+sumMajorCredits(course1, course2);
+sumMinorCredits(course3, course4);
