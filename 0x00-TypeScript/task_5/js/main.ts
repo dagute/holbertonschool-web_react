8bc8c30584bc632ca4a2ae1846b3ecd0d343e392
dagute <dagute28@gmail.com>
2021-02-10 23:16:52 +0000

    update task 5

--- 0x00-TypeScript/task_5/js/main.ts
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -1,25 +1,25 @@
-interface MajorCredits {
+export interface MajorCredits {
   credits: number;
   _majorBrand: void;
 }
 
-interface MinorCredits {
+export interface MinorCredits {
   credits: number;
   _minorBrand: void;
 }
 
-const course1: MajorCredits = { credits: 3 } as MajorCredits;
+/*const course1: MajorCredits = { credits: 3 } as MajorCredits;
 const course2: MajorCredits = { credits: 3 } as MajorCredits;
 const course3: MinorCredits = { credits: 1 } as MinorCredits; 
-const course4: MinorCredits = { credits: 1 } as MinorCredits;
+const course4: MinorCredits = { credits: 1 } as MinorCredits;*/
 
-function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
   return { credits: subject1.credits + subject2.credits} as MajorCredits;
-}
+};
 
-function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
   return { credits: subject1.credits + subject2.credits} as MinorCredits;
-}
+};
 
-sumMajorCredits(course1, course2);
-sumMinorCredits(course3, course4);
+/*sumMajorCredits(course1, course2);
+sumMinorCredits(course3, course4);*/
