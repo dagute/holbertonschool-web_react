796463123cd5d596fdd78c2033ae268adce855f3
dagute <dagute28@gmail.com>
2021-02-10 04:37:46 +0000

    adding ttask 5

---
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -0,0 +1,14 @@
+interface MajorCredits {
+  credits: number;
+  _brand: "Major";
+}
+interface MinorCredits {
+  credits: number;
+  _brand: "Minor";
+}
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): number {
+  return subject1.credits + subject2.credits;
+}; 
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): number {
+  return subject1.credits + subject2.credits;
+}
